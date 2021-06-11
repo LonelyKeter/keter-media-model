743b9bba6f6bb0fc1294b782034a537e7271b7b1
Serg Chernoivanenko <LonelyKeter@gmail.com>
2021-06-11 16:59:19 +0300

    Fixed publicity

--- src/userinfo.rs
+++ src/userinfo.rs
@@ -26,22 +26,22 @@ pub struct AuthorContacts {
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "postgres_query", derive(FromSqlRow))]
 pub struct UserPrivelegies {
-    author: bool,
-    moderator: bool,
-    admin: bool
+    pub author: bool,
+    pub moderator: bool,
+    pub admin: bool
 }
 
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "postgres_query", derive(FromSqlRow))]
 pub struct LoginData {
-    email: String,
-    password: String
+    pub email: String,
+    pub password: String
 }
 
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-#[cfg_attr(feature = "postgres_query", derive(FromSqlRow))]
 pub struct RegisterData {
-    user_name: String,
+    #[serde(rename = "userName", flatten)]
+    pub user_name: String,
     #[serde(rename = "loginData", flatten)]
-    login_data: LoginData
+    pub login_data: LoginData
 }
